0de601507ff5953d404e8e6518f9842ed26d84d3
Jamie Otto <jmotto31@gmail.com>
2022-09-12 23:53:30 -0700

    Readme and comments.js

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Blog-Spot
+# Tech Blog Spot
+
+ ## Description
+  The Tech Blog Spot is an application built a place where developers can publish their blog posts and comment on other developers’ posts as well.  This application follows the MVC paradigm in its architectural structure, uses Handlebars.js as the template language, Sequelize as the ORM, and the express-session npm package for authentication.
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+ The packages required are:
+    <br/>`bcrypt`
+    <br/>`connect-session-sequelize`
+    <br/>`dotenv`
+    <br/>`express`
+    <br/>`express-handlebars`
+    <br/>`express-session`
+    <br/>`mysql2`
+    <br/>`sequelize`
+
+
+  ## Usage
+  
+  To launch this application, click [here](https://ch-14-mvc-blog-spot.herokuapp.com/login).
+
+  ![blog-homepage](assets/blog-homepage-img.png)
+
+  ![blog-login](assets/blog-login-img.png)
+
+  ![blog-signup](assets/blog-signup-img.png)
+  
+  ![blog-dashboard](assets/blog-dashboard.png)
+
+  ## License
+  This application is licensed under [MIT]((https://opensource.org/licenses/MIT)). 
+  <br/> Copyright &copy; 2022 Jamie Otto
+  
+
+  ## Contributors
+  Jamie Otto
+
+  ## Tests 
+  Run `npm test` in the command-line to test using [Jest](https://www.npmjs.com/package/jest).
+
+  ## Questions
+  If you have any questions about this project, please contact [jmotto31@gmail.com](mailto:jmotto31@gmail.com). To view more of my projects, go to [jmotto](https://github.com/jmotto).
+
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,32 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Route "/post/:id"
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        { model: Comment, 
+          include: [User] 
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Route "/login"
 router.get("/login", (req, res) => {
   // If a session exists, redirect the request to the homepage
@@ -65,54 +91,9 @@ router.get("/dashboard", withAuth, async (req, res) => {
   }
 });
 
-// Route "/dashboard/new"
 
 // Route "/dashboard/edit/:id"
 
-// Route "/post/:id"
-router.get("/post/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ["username"],
-        },
-        { model: Comment, 
-          include: [User] 
-        },
-      ],
-    });
 
-    const post = postData.get({ plain: true });
-
-    res.render("post", {
-      ...post,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Prevent non logged in users from viewing the homepage
-
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//     });
-
-//     const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       // Pass the logged in flag to the template
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
