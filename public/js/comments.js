0c74dd5393552182e00407a96e50c6cac4c8cf94
Jamie Otto <jmotto31@gmail.com>
2022-10-01 14:24:03 -0700

    adding comments fixes

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,16 +1,18 @@
 const commentFormHandler = async function (event) {
 	event.preventDefault();
 
-    const post_id = document.querySelector('.new-comment-form').dataset.postid;
-    
-    const comment_description = document.querySelector('#comment_description').value.trim();
-
-    if ( comment_description) {
+    // const post_id = document.querySelector('.new-comment-form').id;
+    const urlArray = window.location.href.split("/");
+    const post_id = urlArray[urlArray.length -1];
+    console.log(post_id);
+    const description = document.querySelector('#comment_description').value.trim();
+    console.log(description);
+    if ( description) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
                 post_id,
-                comment_description,
+                description,
             }),
             headers: {
                 'Content-Type': 'application/json'
